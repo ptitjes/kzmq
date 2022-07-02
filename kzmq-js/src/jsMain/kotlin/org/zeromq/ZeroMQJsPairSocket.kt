86590b8d046f5c9981ab10d2fa6339e99ecb010c
Didier Villevalois <ptitjes@free.fr>
2022-07-02 13:16:47 +0200

    feat(js): add implementations for Pair, Pull/Push, and Request/Reply

---
+++ kzmq-js/src/jsMain/kotlin/org/zeromq/ZeroMQJsPairSocket.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021-2021 Didier Villevalois and Kzmq contributors.
+ * Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package org.zeromq
+
+import org.zeromq.internal.zeromqjs.Pair as ZPair
+
+internal class ZeroMQJsPairSocket internal constructor(
+    override val underlying: ZPair = ZPair(),
+) :
+    ZeroMQJsSocket(Type.PUB),
+    SendSocket by ZeroMQJsSendSocket(underlying),
+    ReceiveSocket by ZeroMQJsReceiveSocket(underlying),
+    PairSocket
---
+++ kzmq-js/src/jsMain/kotlin/org/zeromq/ZeroMQJsReplySocket.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2021-2021 Didier Villevalois and Kzmq contributors.
+ * Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package org.zeromq
+
+import org.zeromq.internal.zeromqjs.Reply as ZReply
+
+internal class ZeroMQJsReplySocket internal constructor(
+    override val underlying: ZReply = ZReply(),
+) :
+    ZeroMQJsSocket(Type.PUB),
+    ReceiveSocket by ZeroMQJsReceiveSocket(underlying),
+    SendSocket by ZeroMQJsSendSocket(underlying),
+    ReplySocket {
+
+    override var routingId: String? by underlying::routingId
+}
