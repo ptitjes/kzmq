55150cf2fb7c60b54682ce83b5ed3b97423cafbb
Didier Villevalois <ptitjes@free.fr>
2022-07-02 13:35:03 +0200

    chore: update copyrights

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2021 Didier Villevalois and Kzmq contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright (c) 2021-2022 Didier Villevalois and Kzmq contributors.
+ * Use of this source code is governed by the Apache 2.0 license.
  */
 
 import java.util.*
