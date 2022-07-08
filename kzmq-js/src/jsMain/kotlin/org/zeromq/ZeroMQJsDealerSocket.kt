a81050701d1105c4ecdc46cc4ffc7fe1ed10f39c
Didier Villevalois <ptitjes@free.fr>
2022-07-08 16:21:28 +0200

    feat(zeromq.js): add missing sockets

---
+++ kzmq-js/src/jsMain/kotlin/org/zeromq/ZeroMQJsDealerSocket.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2021-2022 Didier Villevalois and Kzmq contributors.
+ * Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package org.zeromq
+
+import org.zeromq.internal.zeromqjs.Dealer as ZDealer
+
+internal class ZeroMQJsDealerSocket internal constructor(
+    override val underlying: ZDealer = ZDealer(),
+) :
+    ZeroMQJsSocket(Type.DEALER),
+    SendSocket by ZeroMQJsSendSocket(underlying),
+    ReceiveSocket by ZeroMQJsReceiveSocket(underlying),
+    DealerSocket {
+
+    override var conflate: Boolean by underlying::conflate
+    override var routingId: String? by underlying::routingId
+    override var probeRouter: Boolean by underlying::probeRouter
+}
