c6a00904cda44688455e04816d4d3070409562f9
Didier Villevalois <ptitjes@free.fr>
2022-08-08 23:23:38 +0200

    feat(all): add kzmq-tools subproject to host the throughput test utils

---
+++ kzmq-tools/build.gradle.kts
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2021-2022 Didier Villevalois and Kzmq contributors.
+ * Use of this source code is governed by the Apache 2.0 license.
+ */
+
+plugins {
+    application
+}
+
+val kotlinxCoroutinesVersion: String by project
+val kotlinxCliVersion: String by project
+
+val mingwPath = File(System.getenv("MINGW64_DIR") ?: "C:/msys64/mingw64")
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+        withJava()
+    }
+
+    val hostOs = System.getProperty("os.name")
+
+    val hostTarget = when {
+        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        hostOs.startsWith("Windows") -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+    hostTarget.apply {
+        binaries {
+            executable("throughput") { entryPoint = "org.zeromq.tools.ThroughputKt" }
+        }
+        compilations["main"].enableEndorsedLibs = false
+    }
+
+    sourceSets {
+        all {
+            languageSettings.optIn("kotlin.RequiresOptIn")
+            languageSettings.optIn("kotlinx.coroutines.ExperimentalCoroutinesApi")
+        }
+
+        val commonMain by getting {
+            dependencies {
+                implementation(project(":kzmq-core"))
+                implementation(project(":kzmq-cio"))
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinxCoroutinesVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-cli:$kotlinxCliVersion")
+            }
+        }
+        val jvmMain by getting {
+            dependencies {
+                implementation(project(":kzmq-jeromq"))
+                runtimeOnly("org.slf4j:slf4j-simple:1.7.36")
+            }
+        }
+    }
+}
+
+application {
+    mainClass.set("org.zeromq.tools.ThroughputKt")
+}
---
+++ kzmq-tools/src/nativeMain/kotlin/org/zeromq/tools/dispatcherNative.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2022 Didier Villevalois and Kzmq contributors.
+ * Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package org.zeromq.tools
+
+import kotlinx.coroutines.*
+import platform.linux.*
+import kotlin.math.*
+
+private val nThreads = max(get_nprocs(), 64)
+private val IO_DISPATCHER = newFixedThreadPoolContext(nThreads, "IO")
+
+actual val dispatcher = IO_DISPATCHER
