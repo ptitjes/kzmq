75911464d432561ba969b48dd4d09b193335fef6
Didier Villevalois <ptitjes@free.fr>
2024-07-28 13:45:25 +0200

    chore: update copyright

--- kzmq-cio/src/commonMain/kotlin/org/zeromq/internal/SocketOptions.kt
+++ kzmq-cio/src/commonMain/kotlin/org/zeromq/internal/SocketOptions.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022 Didier Villevalois and Kzmq contributors.
+ * Copyright (c) 2021-2024 Didier Villevalois and Kzmq contributors.
  * Use of this source code is governed by the Apache 2.0 license.
  */
 
