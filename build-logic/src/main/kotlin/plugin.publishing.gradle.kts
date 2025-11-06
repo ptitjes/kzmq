e76cea2f1c5f003b1dc48d57e6a2eb8b92097a54
Didier Villevalois <ptitjes@free.fr>
2025-11-06 16:35:14 +0100

    build: initial publishing setup

---
+++ build-logic/src/main/kotlin/plugin.publishing.gradle.kts
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2022-2024 Didier Villevalois and Kzmq contributors.
+ * Use of this source code is governed by the Apache 2.0 license.
+ */
+
+plugins {
+    id("plugin.library")
+    id("com.vanniktech.maven.publish")
+}
+
+mavenPublishing {
+
+}
+
+publishing {
+
+}
