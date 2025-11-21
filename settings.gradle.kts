997964536145a83c333fccc964edb4669d5c6837
Didier Villevalois <ptitjes@free.fr>
2025-11-21 13:53:03 +0100

    build(all): use foojay toolchain provisioning

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,6 +19,10 @@ dependencyResolutionManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 rootProject.name = "kzmq"
 
 include(":kzmq-core")
