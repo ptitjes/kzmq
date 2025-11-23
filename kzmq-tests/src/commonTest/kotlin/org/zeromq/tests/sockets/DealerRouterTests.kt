f978e6ff5ba5957e6864adf883a8cba2620c8029
Didier Villevalois <ptitjes@free.fr>
2025-11-23 22:03:18 +0100

    test(tests): mute DealerRouterTests.base for 'cio-cio, ipc'

--- kzmq-tests/src/commonTest/kotlin/org/zeromq/tests/sockets/DealerRouterTests.kt
+++ kzmq-tests/src/commonTest/kotlin/org/zeromq/tests/sockets/DealerRouterTests.kt
@@ -23,7 +23,7 @@ val DealerRouterTests by testSuite {
 
     dualContextTest("base", config = {
         // TODO fix when testing more Dealer and Router logic
-        skip("cio-cio, inproc")
+        skip("cio-cio, inproc", "cio-cio, ipc")
     }) { ctx1, ctx2, protocol ->
         val dealerCount = 2
         val routerCount = 3
